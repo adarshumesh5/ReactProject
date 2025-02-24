70ca9adc054c05d3c36f565c8ab985c65743b596
Adarsh <adarshumesh5@gmail.com>
2025-02-23 19:35:26 -0800

    Added Education and Work Experience with formatting

--- reactproject/src/components/Education.js
+++ reactproject/src/components/Education.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import './Section.css';
-
-
+import { Typography, Grid2 } from '@mui/material';
 
 const Education = () => {
   return (
-   
-
     <section className="section education">
-      <h2>Education</h2>
+      <Typography variant='h5' align='left'><b>Education</b></Typography>
       <div className="section-content">
-        <div className="item">
-          <h3>University Name</h3>
-          <p>Degree, Major</p>
-          <p>Year of Graduation</p>
-        </div>
+        <Grid2 container spacing={2}>
+          <Grid2 item xs={12}>
+            <Typography variant='h6' align='left'><b>California State University, Northridge</b></Typography>
+            <Typography variant='body1' align='left'>Master of Science - Engineering Management</Typography>
+            <Typography variant='body2' align='left'>Fall 2023 - Ongoing</Typography>
+            <Typography variant='body2' align='left'><b>CGPA:</b> 3.43/4</Typography>
+            <Typography variant='body2' align='left'><b>Coursework:</b> Product Design, Leadership of Engineering Professionals, Operations Management, Lean Manufacturing, Decision Tools, Tech Entrepreneurship, Data Analytics, Data Visualization, Engineering Economics, Project Management</Typography>
+          </Grid2>
+          <Grid2 item xs={12}>
+            <Typography variant='h6' align='left'><b>CMR Institute of Technology</b></Typography>
+            <Typography variant='body1' align='left'>Bachelor of Engineering in Electronics and Communication</Typography>
+            <Typography variant='body2' align='left'>Aug 2016 - Aug 2020</Typography>
+            <Typography variant='body2' align='left'><b>CGPA:</b> 6.71/10</Typography>
+            <Typography variant='body2' align='left'><b>Coursework:</b> Network Theory, Programming in C, C++, and Java, Digital Signal Processing, Control Systems</Typography>
+          </Grid2>
+        </Grid2>
       </div>
     </section>
-    
-
   );
 };
 
--- reactproject/src/components/WorkExperience.js
+++ reactproject/src/components/WorkExperience.js
@@ -1,16 +1,57 @@
 import React from 'react';
 import './Section.css';
+import { Typography, Grid2 } from '@mui/material';
 
 const WorkExperience = () => {
   return (
-    <section className="section work-experience">
-      <h2>Work Experience</h2>
-      <div className="section-content">
-        <div className="item">
-          <h3>Company Name</h3>
-          <p>Position</p>
-          <p>Duration</p>
-        </div>
+    <section className="section WorkExperience">
+      <Typography variant='h5' align='left'><b>Work Experience</b></Typography>
+      <div >
+        <Grid2 container spacing={2}>
+          <Grid2 item xs={12}>
+            <Typography variant='h6' align='left'><b>GlobalLogic India Private Limited</b></Typography>
+            <Typography variant='body1' align='left'>Senior Software Engineer</Typography>
+            <Typography variant='body2' align='left'>Dec 2020 - July 2023</Typography>
+            <Typography variant='body2' align='left'><b>Key Responsibilities:</b></Typography>
+            <ul>
+              <li>
+                <Typography variant='body2' align='left'>Developed modular, user-interactive web applications using React.js, Node.js, and Java Spring Boot Microservices.</Typography>
+              </li>
+              <li>
+                <Typography variant='body2' align='left'>Built and optimized RESTful APIs to improve backend performance and maintainability.</Typography>
+              </li>
+              <li>
+                <Typography variant='body2' align='left'>Led Scrum ceremonies as Scrum Master, facilitating Sprint Planning, Daily Standups, and Retrospectives.</Typography>
+              </li>
+              <li>
+                <Typography variant='body2' align='left'>Collaborated with Product Managers and cross-functional teams to define system requirements and align development with business goals.</Typography>
+              </li>
+              <li>
+                <Typography variant='body2' align='left'>Refactored legacy codebases, improving maintainability and reducing technical debt by 25%.</Typography>
+              </li>
+              <li>
+                <Typography variant='body2' align='left'>Developed comprehensive unit, integration, regression, and smoke tests utilizing Jest, Mocha, JUnit, and Cypress, resulting in an impressive 95% test coverage.</Typography>
+              </li>
+              <li>
+                <Typography variant='body2' align='left'>Executed performance testing with Apache JMeter to validate the application's capability to support over 5,000 concurrent users.</Typography>
+              </li>
+            </ul>
+          </Grid2>
+          <Grid2 item xs={12}>
+            <Typography variant='h6' align='left'><b>IFB Automotive Private Limited</b></Typography>
+            <Typography variant='body1' align='left'>Intern</Typography>
+            <Typography variant='body2' align='left'>Jan 2020 - Feb 2020</Typography>
+            <Typography variant='body2' align='left'><b>Key Responsibilities:</b></Typography>
+            <ul>
+              <li>
+                <Typography variant='body2' align='left'>Contributed to the analysis of Network attacks and created comprehensive documentation.</Typography>
+              </li>
+              <li>
+                <Typography variant='body2' align='left'>Identified and reported vulnerabilities by performing random checks on systems.</Typography>
+              </li>
+            </ul>
+          </Grid2>
+        </Grid2>
       </div>
     </section>
   );
